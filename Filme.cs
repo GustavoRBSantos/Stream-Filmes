3f7357966305f59cfa8d3c22b72d3eca47303abc
Gustavo <gustavorodrigues978047@gmail.com>
2026-06-17 19:51:15 -0300

    feat: cria classe Filme

---
+++ Filme.cs
@@ -0,0 +1,16 @@
+﻿public class Filme
+{
+    public string titulo;
+    public string genero;
+    public int anoLancamento;
+    public int classificacaoIndicativa;
+
+    public void ExibirInformarcoes()
+    {
+        Console.WriteLine("\n === Cadastro Filme ===");
+        Console.WriteLine($"Nome: {titulo}");
+        Console.WriteLine($"Genero: {genero}");
+        Console.WriteLine($"Ano de lançamento: {anoLancamento}");
+        Console.WriteLine($"Classificação: {classificacaoIndicativa}");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Filme filme = new Filme();
+filme.titulo = "Avatar";
+filme.genero = "Ficção científica";
+filme.anoLancamento = 2009;
+filme.classificacaoIndicativa = 12;
+
+filme.ExibirInformarcoes();
